ccc9824fdeafac0275138c316615d57050832e7d
Moussa Azzoug <115167027+StBiccy@users.noreply.github.com>
2023-02-26 02:11:54 +0000

    pain and suffering
    Physics are implemented. But not properly... pain and suffering

--- Assets/Inputactions/Character Inputs.cs
+++ Assets/Inputactions/Character Inputs.cs
@@ -31,24 +31,15 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
                     ""name"": ""Move"",
                     ""type"": ""Value"",
                     ""id"": ""041cf818-ebb2-4213-b395-c9602875e4f1"",
-                    ""expectedControlType"": ""Vector2"",
+                    ""expectedControlType"": ""Axis"",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
                 },
                 {
-                    ""name"": ""Look"",
-                    ""type"": ""Value"",
-                    ""id"": ""581a6465-f514-46b0-9c03-42c84555b5b4"",
-                    ""expectedControlType"": ""Vector2"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": true
-                },
-                {
-                    ""name"": ""Fire"",
+                    ""name"": ""Jump"",
                     ""type"": ""Button"",
-                    ""id"": ""dc3300ad-b14f-42be-9392-1cc5f821ef4e"",
+                    ""id"": ""7401fddc-2a20-4bf3-aac4-4f1d7f86a0a3"",
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """",
@@ -57,9 +48,9 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
             ],
             ""bindings"": [
                 {
-                    ""name"": ""WASD"",
+                    ""name"": ""Direction"",
                     ""id"": ""00ca640b-d935-4593-8157-c05846ea39b3"",
-                    ""path"": ""Dpad"",
+                    ""path"": ""1DAxis"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
@@ -68,30 +59,8 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
                     ""isPartOfComposite"": false
                 },
                 {
-                    ""name"": ""up"",
+                    ""name"": ""Negative"",
                     ""id"": ""e2062cb9-1b15-46a2-838c-2f8d72a0bdd9"",
-                    ""path"": ""<Keyboard>/w"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""down"",
-                    ""id"": ""320bffee-a40b-4347-ac70-c210eb8bc73a"",
-                    ""path"": ""<Keyboard>/s"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""left"",
-                    ""id"": ""d2581a9b-1d11-4566-b27d-b92aff5fabbc"",
                     ""path"": ""<Keyboard>/a"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -101,8 +70,8 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
                     ""isPartOfComposite"": true
                 },
                 {
-                    ""name"": ""right"",
-                    ""id"": ""fcfe95b8-67b9-4526-84b5-5d0bc98d6400"",
+                    ""name"": ""Positive"",
+                    ""id"": ""320bffee-a40b-4347-ac70-c210eb8bc73a"",
                     ""path"": ""<Keyboard>/d"",
                     ""interactions"": """",
                     ""processors"": """",
@@ -113,89 +82,12 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": """",
-                    ""id"": ""c1f7a91b-d0fd-4a62-997e-7fb9b69bf235"",
-                    ""path"": ""<Gamepad>/rightStick"",
+                    ""id"": ""d526b7a9-04dd-4994-8f82-161a1899c73b"",
+                    ""path"": ""<Keyboard>/space"",
                     ""interactions"": """",
                     ""processors"": """",
-                    ""groups"": "";Gamepad"",
-                    ""action"": ""Look"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""8c8e490b-c610-4785-884f-f04217b23ca4"",
-                    ""path"": ""<Pointer>/delta"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse;Touch"",
-                    ""action"": ""Look"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""3e5f5442-8668-4b27-a940-df99bad7e831"",
-                    ""path"": ""<Joystick>/{Hatswitch}"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Joystick"",
-                    ""action"": ""Look"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""143bb1cd-cc10-4eca-a2f0-a3664166fe91"",
-                    ""path"": ""<Gamepad>/rightTrigger"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Gamepad"",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""05f6913d-c316-48b2-a6bb-e225f14c7960"",
-                    ""path"": ""<Mouse>/leftButton"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""886e731e-7071-4ae4-95c0-e61739dad6fd"",
-                    ""path"": ""<Touchscreen>/primaryTouch/tap"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Touch"",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""ee3d0cd2-254e-47a7-a8cb-bc94d9658c54"",
-                    ""path"": ""<Joystick>/trigger"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Joystick"",
-                    ""action"": ""Fire"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""8255d333-5683-4943-a58a-ccb207ff1dce"",
-                    ""path"": ""<XRController>/{PrimaryAction}"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""XR"",
-                    ""action"": ""Fire"",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -784,8 +676,7 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
-        m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
-        m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -858,15 +749,13 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
     private readonly InputActionMap m_Player;
     private IPlayerActions m_PlayerActionsCallbackInterface;
     private readonly InputAction m_Player_Move;
-    private readonly InputAction m_Player_Look;
-    private readonly InputAction m_Player_Fire;
+    private readonly InputAction m_Player_Jump;
     public struct PlayerActions
     {
         private @CharacterInputs m_Wrapper;
         public PlayerActions(@CharacterInputs wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
-        public InputAction @Look => m_Wrapper.m_Player_Look;
-        public InputAction @Fire => m_Wrapper.m_Player_Fire;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -879,12 +768,9 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
                 @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
                 @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
                 @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
-                @Look.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
-                @Look.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
-                @Look.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
-                @Fire.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
-                @Fire.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
-                @Fire.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnFire;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -892,12 +778,9 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
                 @Move.started += instance.OnMove;
                 @Move.performed += instance.OnMove;
                 @Move.canceled += instance.OnMove;
-                @Look.started += instance.OnLook;
-                @Look.performed += instance.OnLook;
-                @Look.canceled += instance.OnLook;
-                @Fire.started += instance.OnFire;
-                @Fire.performed += instance.OnFire;
-                @Fire.canceled += instance.OnFire;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
             }
         }
     }
@@ -1055,8 +938,7 @@ public partial class @CharacterInputs : IInputActionCollection2, IDisposable
     public interface IPlayerActions
     {
         void OnMove(InputAction.CallbackContext context);
-        void OnLook(InputAction.CallbackContext context);
-        void OnFire(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -5,24 +5,75 @@ using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
-    private CharacterInputs input;
-    private Vector2 direction;
+    private CharacterInputs input; // Input class reference
+
+    private Rigidbody2D rb; // Player's rigidbody refrence
+
+    [Header("Ground Check Values")]
+    [SerializeField] private CapsuleCollider2D coll; // Player's collison refrence
+    [SerializeField] private LayerMask groundLayers; // Ground layers refrence
+    [SerializeField] private bool groundCheckGizmo; // Check if we want to see gizmo for ground checks
+    private Color groundGizmo = Color.red; //Colour value of the ground check gizmo
+
+    [Header("Movement values")]
+    [SerializeField, Range(0.0f, 20.0f)] float runAccel; // Acceleration rate of the player
+    [SerializeField, Range(0.0f, 20.0f)] private float maxRunSpeed; // desire speed for the player to move at
+    private float runAccelAmount; // Represent the current acceleration amount
+    [SerializeField, Range(0.0f, 20.0f)] private float runDeccel; // Decceleration rate of the player
+    private float runDeccelAmount; // Represents the current decceleration amount
+
+    private float direction; // The directoin the player is moving in
+
+    [Header("Jump values")]
+    [SerializeField, Range(0.0f, 20.0f)] private float jumpHight; //The hight of the player's jump
+    [SerializeField, Range(0.0f, 20.0f)] private float jumpTimeToApex; //Time between applying jump and reaching the apex, also used it gravityStrength, jumpForce
+    private float jumpForce; // The force applied to the player (upwards) when they jump
+    private float gravityStrength; // The downward fource (gravity) use in jumpForce, gravityScale
+    private float gravityScale; // represents the gravity scale this is to be set to
+    private bool jumping = false;// check if the player is jumping <----
 
     private void Awake()
     {
         input = new CharacterInputs();
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<CapsuleCollider2D>();
+    }
+
+    private void Start()
+    {
+        gravityStrength = -(2 * jumpHight) / (jumpTimeToApex * jumpTimeToApex);
+        gravityScale = gravityStrength / Physics2D.gravity.y;
+
+        jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
+
+        runAccelAmount = (50 * runAccel) / maxRunSpeed;
+        runDeccelAmount = (50* runDeccel) / maxRunSpeed;
+
+
+        SetGravityScale(gravityScale);
+    }
+
+    private void SetGravityScale (float scale)
+    {
+        rb.gravityScale = scale;
+    }
+
+    private void OnValidate()
+    {
+        gravityStrength = -(2 * jumpHight) / (jumpTimeToApex * jumpTimeToApex);
+
+
+
+        jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
     }
 
     private void OnEnable()
     {
         input.Enable();
+
+        //Performes when move key is down, and stops when move key is let go
         input.Player.Move.performed += OnMovement;
         input.Player.Move.canceled += OnMovementCanceled;
-    }
-
-    private void FixedUpdate()
-    {
-        
     }
 
     private void OnDisable()
@@ -32,13 +83,94 @@ public class CharacterController : MonoBehaviour
         input.Player.Move.canceled -= OnMovementCanceled;
     }
 
+    private void FixedUpdate()
+    {
+        Movement();
+
+        if (jumping && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.performed) 
+        {
+            jumping = true;
+
+            Debug.Log(jumping);
+        }
+        if(context.canceled) 
+        {
+            jumping= false;
+
+            Debug.Log(jumping);
+        }
+    }
+
     private void OnMovement(InputAction.CallbackContext value)
     {
-        direction = value.ReadValue<Vector2>();
+        direction = value.ReadValue<float>();
     }
 
     private void OnMovementCanceled(InputAction.CallbackContext value)
     {
-        direction = Vector2.zero;
+        direction = 0;
     }
+
+    //Preforms player movement
+    private void Movement()
+    {
+        float targetSpeed = maxRunSpeed * direction;
+
+        // I currently don't think this matters
+        targetSpeed = Mathf.Lerp(rb.velocity.x, targetSpeed, 1);
+
+        float accelRate;
+
+        if(IsGrounded()) 
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? runAccelAmount : runDeccelAmount;
+
+        //finds how much fource needs to be applied for acceleartion.
+        //first finds differnce between target speed and current speed,
+        //next multiplies the diffrence by the accelaraction rate to find how much fource needs to be added.
+        float speedDif = targetSpeed - rb.velocity.x;
+        float movement = speedDif * runAccelAmount;
+
+        //Applies the movement value as a fource on the x axis
+        rb.AddForce(movement * Vector2.right, ForceMode2D.Force);
+    }
+
+    private void Jump()
+    {
+        float force = jumpForce;
+        if(rb.velocity.y < 0)
+            force -= rb.velocity.y;
+
+        rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(groundCheckGizmo)
+        {
+
+        }
+    }
+
+    //Checks if the player is grounded
+    private bool IsGrounded()
+    {
+        bool result = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size , 0, Vector2.down, .01f, groundLayers);
+        if (result)
+            groundGizmo = Color.green;
+        else 
+            groundGizmo = Color.red;
+
+        return result;
+    }
+
+    
+
 }
