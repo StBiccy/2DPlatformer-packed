15fd7378d356205988515b10a22d7b35848c7e41
Moussa Azzoug <115167027+StBiccy@users.noreply.github.com>
2023-03-15 14:03:24 +0000

    enemy searches and moves towards player
    make the enemy loo for an move towards the player

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -18,14 +18,17 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private GameObject projectile;
     private float bulletCountDown;
 
-    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float patrolSpeed = 1f;
+    [SerializeField] private float chaseSpeed = 1f;
     [SerializeField] private float lookDistance;
+    [SerializeField] private LayerMask observableLayers;
+    [SerializeField] private bool debugRay;
 
-    bool isAiming = false;
-    bool isPatroling = true;
-
-    bool flip = false;
-    bool lookingRight = true;
+    private bool moveTowardsPlayer = false;
+    private bool canNotMove = false;
+    private bool isAiming = false;
+    private bool isPatroling = true;
+    private bool lookingRight = true;
 
     private void Awake()
     {
@@ -41,102 +44,69 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (flip) { FlipSelf(); }
+        Vector2 direction = target.transform.position - this.transform.position;
 
         if (isAiming)
         {
-            Quaternion rotation = new Quaternion(0, 0, 0, 0);
-
-            bulletCountDown -= Time.deltaTime;
-
-
-            if (target.transform.position.x < transform.position.x && lookingRight)
-            {
-                flip = true;
-                
-            }
-            else if (target.transform.position.x > transform.position.x && !lookingRight)
-            {
-                flip = true;
-            }
-
-            
-
-            if (lookingRight)
-            {
-                Vector3 direction = target.transform.position - transform.position;
-                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, Time.deltaTime * lookSpeed);
-            }
-            else if (!lookingRight)
-            {
-                Vector3 direction = target.transform.position - transform.position;
-                float angle = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
-                rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, Time.deltaTime * lookSpeed);
-            }
-
-            if (transform.rotation == rotation)
-            {
-                if (bulletCountDown <= 0)
-                {
-                    Vector3 targetForward = pivot.transform.rotation * Vector3.forward;
-                    Vector3 targetUp = pivot.transform.rotation * Vector3.up;
-
-                    var bulletRef = Instantiate(projectile, firePoint.transform.position, pivot.transform.rotation);
-
-                    if (lookingRight)
-                        bulletRef.GetComponent<Projectile>().direction = 1;
-                    else
-                        bulletRef.GetComponent<Projectile>().direction = -1;
-
-                    bulletCountDown = bulletCoolDown;
-                }
-            }
+            Aim(direction);
         }
 
-        if (isPatroling)
-        {
-            Vector2 directon = new Vector2(transform.position.x - target.transform.position.x, transform.position.x - target.transform.position.x).normalized;
-            /*float distace = Vector2.Distance(this.transform.position, target.transform.position);*/
-
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, directon, lookDistance);
-
-            Debug.DrawLine(transform.position, transform.position + new Vector3 (directon.x, directon.y, 0) * lookDistance, Color.green, Mathf.Infinity);
-
-            if(hit.collider == target.GetComponent<Collider2D>())
-            {
-                print("yes");
-            }
-        }
+        playerSearch(direction);
     }
 
     private void FixedUpdate()
     {
         if (isPatroling)
         {
+            patrol();
+        }
+
+        else if (moveTowardsPlayer)
+        {
             if (lookingRight)
             {
+                if (target.transform.position.x < this.transform.position.x) { FlipSelf(); }
 
-                enemyRB.velocity = new Vector2(moveSpeed, 0);
+                if (!canNotMove) { enemyRB.velocity = new Vector2(chaseSpeed, 0); }
+                else { enemyRB.velocity = Vector2.zero; }
             }
             else
             {
-                enemyRB.velocity = new Vector2(-moveSpeed, 0);
+                if (target.transform.position.x > this.transform.position.x) { FlipSelf(); }
+
+                if (!canNotMove) { enemyRB.velocity = new Vector2(-chaseSpeed, 0); }
+                else { enemyRB.velocity = Vector2.zero; }
             }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        flip = true;
+        if(!moveTowardsPlayer) { FlipSelf(); }
+        else { canNotMove = true; }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(moveTowardsPlayer) { canNotMove = false; }
+    }
+
+    private void patrol()
+    {
+        if (lookingRight)
+        {
+
+            enemyRB.velocity = new Vector2(patrolSpeed, 0);
+        }
+        else
+        {
+            enemyRB.velocity = new Vector2(-patrolSpeed, 0);
+        }
     }
 
     void FlipSelf()
     {
-        flip = false;
-        transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, -transform.rotation.z,transform.rotation.w);
+        transform.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, -transform.rotation.z, transform.rotation.w);
         lookingRight = lookingRight ? false : true;
 
         if (lookingRight)
@@ -148,6 +118,71 @@ public class EnemyController : MonoBehaviour
         {
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             return;
+        }
+    }
+
+    private void Aim(Vector2 dir)
+    {
+        Quaternion rotation = new Quaternion(0, 0, 0, 0);
+
+        bulletCountDown -= Time.deltaTime;
+
+        // Checks if the player is now on the other side of the enemy and filps them if so
+        if (target.transform.position.x < transform.position.x && lookingRight) { FlipSelf(); }
+        else if (target.transform.position.x > transform.position.x && !lookingRight) { FlipSelf();}
+
+
+        // rotates the gun in the direction of the player
+        if (lookingRight)
+        {
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            pivot.transform.rotation = Quaternion.RotateTowards(pivot.transform.rotation, rotation, Time.deltaTime * lookSpeed);
+        }
+        else if (!lookingRight)
+        {
+            Vector3 direction = target.transform.position - transform.position;
+            float angle = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg;
+            rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            pivot.transform.rotation = Quaternion.RotateTowards(pivot.transform.rotation, rotation, Time.deltaTime * lookSpeed);
+        }
+
+        //fires the bullet
+        if (transform.rotation == rotation && bulletCountDown <= 0)
+        {
+
+            Vector3 targetForward = pivot.transform.rotation * Vector3.forward;
+            Vector3 targetUp = pivot.transform.rotation * Vector3.up;
+
+            var bulletRef = Instantiate(projectile, firePoint.transform.position, pivot.transform.rotation);
+
+            if (lookingRight)
+                bulletRef.GetComponent<Projectile>().direction = 1;
+            else
+                bulletRef.GetComponent<Projectile>().direction = -1;
+
+            bulletCountDown = bulletCoolDown;
+
+        }
+    }
+
+    private void playerSearch(Vector2 dir)
+    {
+        Vector2 fromPosition = this.transform.position;
+        Vector2 toPosition = target.transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, dir, lookDistance, observableLayers);
+
+        if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Character"))
+        {
+            moveTowardsPlayer = true;
+            isPatroling = false;
+        }
+
+        if (debugRay)
+        {
+            Debug.DrawRay(this.transform.position, dir.normalized * hit.distance, Color.green);
+            Debug.DrawRay(hit.point, dir.normalized * (lookDistance - hit.distance), Color.red);
         }
     }
 }
